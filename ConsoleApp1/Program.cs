3b535cdc18117627bed6483b17c3a7de6835094b
KIE_Admin <KIE_Admin@WZR-C24-13>
2025-03-03 15:22:26 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("witaj świecie"); //wypisuje komentarz tu
+            Console.ReadKey(true);
+            /*
+            Console.ReadKey(true);
+            Console.ReadKey(true);
+            Console.ReadKey(true);
+            */
+
+        }
+    }
+}
