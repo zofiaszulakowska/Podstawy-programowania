23c3cffc40f67e9e5d073da5933b3ba4a7785bf8
KIE_Admin <z.szulakowska.985@studms.ug.edu.pl>
2025-03-03 15:34:10 +0100

    koniec zadania 1

---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wypisujący na ekranie tekst "Witaj Świecie".
+            Console.WriteLine("Witaj Świecie");
+            Console.ReadKey(true);
+        }
+    }
+}
