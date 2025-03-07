1cf1811210a5bad01a1bf2a7e46962ee51fd24c0
zofiaszulakowska <KIE_Admin@WZR-C25-05>
2025-03-07 14:24:09 +0100

    koniec

---
+++ zadanie 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_6 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby i wypisujący na ekranie ich sumę, różnicę, iloczyn i iloraz
+            double a, b;
+            Console.WriteLine("Podaj a i b:");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        
+            Console.WriteLine("Wynik dodawania:{0}", a+b);
+            Console.WriteLine("Wynik odejmowania:{0}", a-b);
+            Console.WriteLine("Wynik mnozenia:{0}", a*b);
+            if (b==0)
+            {
+                Console.WriteLine("Nie mozna dzielic przez 0");
+
+            }
+            else
+            {
+                Console.WriteLine("Wynik dzielenia {0}", a / b);
+            }
+                Console.WriteLine("Wynik dzielenia:{0}", a / b);
+        }
+    }
+}
---
+++ zadanie 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący dwie liczby całkowite i wypisujący na ekranie iloraz tych liczb z uwzględnieniem części całkowitej i reszty z dzielenia
+            int a, b;
+            Console.WriteLine("Podaj a");
+                a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wynik dzielenia z reszta :{0} reszta {1} ", a/b, a % b);
+                Console.ReadLine();
+
+        }
+    }
+}
